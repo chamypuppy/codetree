bba10f78cfedd755c2b302d5980b9d5d970ed8b3
푸딩비 <91423376+chamypuppy@users.noreply.github.com>
2025-05-09 12:17:33 +0900

    Title: 굉장한 수, Time: 178ms, Memory: 15MB, Status: Passed - Codetree

--- 250509/굉장한 수/amazing-number.js
+++ 250509/굉장한 수/amazing-number.js
@@ -2,7 +2,7 @@ const fs = require("fs");
 const n = +fs.readFileSync(0).toString().trim();
 let bool = 0;
 
-if(n%2 == 1 || n%2 == 0 && n%5 ==0) bool = 1;
+if(n%2 == 1 && n%3 == 0 || n%2 == 0 && n%5 ==0) bool = 1;
 else bool;
 
 console.log(Boolean(bool));
