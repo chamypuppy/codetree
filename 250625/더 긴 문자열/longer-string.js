4ee4d706f49869daaa41a4965693e178769bd6d0
푸딩비 <91423376+chamypuppy@users.noreply.github.com>
2025-06-25 13:21:40 +0900

    Title: 더 긴 문자열, Time: 176ms, Memory: 15MB, Status: Passed - Codetree

--- 250625/더 긴 문자열/longer-string.js
+++ 250625/더 긴 문자열/longer-string.js
@@ -1,8 +1,10 @@
 const fs = require("fs");
-const str = fs.readFileSync(0).toString().trim().split(" ");
+//const str = fs.readFileSync(0).toString().trim().split(" ");
+const [str1, str2] = fs.readFileSync(0).toString().trim().split(" ");
 
-const str1 = str[0];
-const str2 = str[1];
+// const str1 = str[0];
+// const str2 = str[1];
+
 
 if(str1.length < str2.length) console.log(str2, str2.length);
 else if (str1.length > str2.length) console.log(str1, str1.length);
